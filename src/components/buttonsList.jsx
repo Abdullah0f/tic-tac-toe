5d6229f85ebe18111ccd1879243bbec9177c4106
Abdullah Fahad <abdullah84f@gmail.com>
2023-02-04 23:36:37 +0300

    improvment on buttons style

--- src/components/buttonsList.jsx
+++ src/components/buttonsList.jsx
@@ -1,32 +1,30 @@
 import Button from "./button";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import DropList from "./dropList";
 const ButtonsList = ({ buttons }) => {
   return (
-    <ButtonGroup aria-label="Basic example">
-      {buttons.map((button) =>
-        button.type === "dropList" ? (
-          <DropList
-            items={button.items}
-            label={button.label}
-            onSelect={button.onSelect}
-            theme={button.theme}
-            active={button.active}
-          />
-        ) : (
-          <Button
-            key={button.name}
-            label={button.label}
-            onClick={button.onClick}
-            theme={button.theme}
-          />
-        )
-      )}
-    </ButtonGroup>
-
-    // </div>
+    <div className="button-group mt-2">
+      <ButtonGroup aria-label="Button-group">
+        {buttons.map((button) =>
+          button.type === "dropList" ? (
+            <DropList
+              items={button.items}
+              label={button.label}
+              onSelect={button.onSelect}
+              theme={button.theme}
+              active={button.active}
+            />
+          ) : (
+            <Button
+              key={button.name}
+              label={button.label}
+              onClick={button.onClick}
+              theme={button.theme}
+            />
+          )
+        )}
+      </ButtonGroup>
+    </div>
   );
 };
 
